662170b428c9859c66582439105fb4a6f6b4b192
c0b21047 <c0b2104754@edu.teu.ac.jp>
2022-10-04 14:20:21 +0900

    calc_2

--- ex02/calc.py
+++ ex02/calc.py
@@ -4,4 +4,15 @@ import tkinter.messagebox as tkm
 root = tk.Tk()
 root.geometry("300x500")
 
+x = 0
+y = 0
+for i in range(9,-1,-1):
+    button = tk.Button(root,text=i,width=4,height=2,font=("",30))
+    button.grid(row=y, column=x)
+    if x >= 2:
+        x = 0
+        y += 1
+    else:
+        x += 1
+
 root.mainloop()
