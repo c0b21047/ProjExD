1a2713a7802ecb66682c9748cf4e25352385d2d4
c0b21047 <c0b2104754@edu.teu.ac.jp>
2022-10-04 14:33:15 +0900

    calc_4

--- ex02/calc.py
+++ ex02/calc.py
@@ -9,14 +9,17 @@ def botton_click(event):
 root = tk.Tk()
 root.geometry("300x500")
 
+entry = tk.Entry(root,width=10,justify="right",font=("Times New Roman",40))
+entry.grid(row=0,column=0,columnspan=3)
+
 x = 0
-y = 0
+y = 1
 max_x = 2
 for i in range(9,-1,-1):
     button = tk.Button(root,text=i,width=4,height=2,font=("",30))
     button.bind("<1>",botton_click)
     button.grid(row=y, column=x)
-    if x >= max_x:
+    if x>= max_x:
         x = 0
         y += 1
     else:
