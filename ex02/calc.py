935388b7a572015b6db46b5b92441d63aeec14c2
c0b21047 <c0b2104754@edu.teu.ac.jp>
2022-10-04 14:55:22 +0900

    calc_7

--- ex02/calc.py
+++ ex02/calc.py
@@ -4,7 +4,16 @@ import tkinter.messagebox as tkm
 def button_click(event):
     btn = event.widget
     txt = btn["text"]
-    entry.insert(tk.END,txt)
+    try:
+        if txt == "=":
+            formula = entry.get()
+            ans = eval(formula)
+            entry.delete(0,tk.END)
+            entry.insert(tk.END,ans)
+        else:
+            entry.insert(tk.END,txt)
+    except:
+        None
 
 root = tk.Tk()
 root.geometry("300x500")
@@ -12,10 +21,12 @@ root.geometry("300x500")
 entry = tk.Entry(root,width=10,justify="right",font=("Times New Roman",40))
 entry.grid(row=0,column=0,columnspan=3)
 
+num = [i for i in range(9,-1,-1)]
+ope = ["+","="]
 x = 0
 y = 1
 max_x = 2
-for i in range(9,-1,-1):
+for i in (num+ope):
     button = tk.Button(root,text=i,width=4,height=2,font=("",30))
     button.bind("<1>",button_click)
     button.grid(row=y, column=x)
@@ -24,10 +35,5 @@ for i in range(9,-1,-1):
         y += 1
     else:
         x += 1
-button = tk.Button(root,text="+",width=4,height=2,font=("",30))
-button.bind("<1>",button_click)
-button.grid(row=y,column=x)
-x += 1
-
 
 root.mainloop()
