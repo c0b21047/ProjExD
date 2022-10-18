27afe5d8c9a4c5aeb905aae9d6a612732df604b8
c0b21047 <c0b2104754@edu.teu.ac.jp>
2022-10-18 14:12:17 +0900

    練習問題03-2

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,6 +3,8 @@ import tkinter.messagebox as tkm
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("迷えるこうかとん")
-    
+    root.title("迷えるこうかとん") #練習1
+    canv = tk.Canvas(root,width=1500,height=900,bg="black")
+    canv.pack() #練習2
+
     root.mainloop()
