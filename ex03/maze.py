e1a2214add2bda3004278da0db83e2150d7384ce
c0b21047 <c0b2104754@edu.teu.ac.jp>
2022-10-18 14:21:53 +0900

    練習問題03-4,5

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
+def key_down(event):
+    global key
+    key = event.keysym #練習5
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん") #練習1
@@ -11,5 +15,8 @@ if __name__ == "__main__":
     tori = tk.PhotoImage(file="fig/0.png")
     cx, cy = 300, 400
     canv.create_image(cx, cy, image=tori, tag="tori") #練習3
+
+    key = "" #練習4
+    root.bind("<KeyPress>",key_down)
 
     root.mainloop()
