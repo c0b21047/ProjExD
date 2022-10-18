74f91e6b976dcf9dd9354c910d9171e2b57e8d5a
c0b21047 <c0b2104754@edu.teu.ac.jp>
2022-10-18 14:18:16 +0900

    練習問題03-3

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,7 +4,12 @@ import tkinter.messagebox as tkm
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん") #練習1
+    
     canv = tk.Canvas(root,width=1500,height=900,bg="black")
     canv.pack() #練習2
+
+    tori = tk.PhotoImage(file="fig/0.png")
+    cx, cy = 300, 400
+    canv.create_image(cx, cy, image=tori, tag="tori") #練習3
 
     root.mainloop()
